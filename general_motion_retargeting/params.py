59031579f1ae3e824504f24f0f3ef509d687d6ba
Saminda Abeyruwan <samindaa@gmail.com>
2026-07-05 15:03:48 -0700

    apptroink apollo 1 retargeting

--- general_motion_retargeting/params.py
+++ general_motion_retargeting/params.py
@@ -6,7 +6,9 @@ ASSET_ROOT = HERE / ".." / "assets"
 
 ROBOT_XML_DICT = {
     "unitree_g1": ASSET_ROOT / "unitree_g1" / "g1_mocap_29dof.xml",
-    "unitree_g1_with_hands": ASSET_ROOT / "unitree_g1" / "g1_mocap_29dof_with_hands.xml",
+    "unitree_g1_with_hands": ASSET_ROOT
+    / "unitree_g1"
+    / "g1_mocap_29dof_with_hands.xml",
     "unitree_h1": ASSET_ROOT / "unitree_h1" / "h1.xml",
     "unitree_h1_2": ASSET_ROOT / "unitree_h1_2" / "h1_2_handless.xml",
     "booster_t1": ASSET_ROOT / "booster_t1" / "T1_serial.xml",
@@ -22,12 +24,17 @@ ROBOT_XML_DICT = {
     "pnd_adam_lite": ASSET_ROOT / "pnd_adam_lite" / "scene.xml",
     "tienkung": ASSET_ROOT / "tienkung" / "mjcf" / "tienkung.xml",
     "pal_talos": ASSET_ROOT / "pal_talos" / "talos.xml",
-    "fourier_gr3": ASSET_ROOT / "fourier_gr3v2_1_1" / "mjcf" / "gr3v2_1_1_dummy_hand.xml",
+    "fourier_gr3": ASSET_ROOT
+    / "fourier_gr3v2_1_1"
+    / "mjcf"
+    / "gr3v2_1_1_dummy_hand.xml",
+    # from mujoco_menagerie
+    "apptronik_apollo": ASSET_ROOT / "apptronik_apollo" / "scene.xml",
 }
 
 IK_CONFIG_DICT = {
     # offline data
-    "smplx":{
+    "smplx": {
         "unitree_g1": IK_CONFIG_ROOT / "smplx_to_g1.json",
         "unitree_g1_with_hands": IK_CONFIG_ROOT / "smplx_to_g1.json",
         "unitree_h1": IK_CONFIG_ROOT / "smplx_to_h1.json",
@@ -46,7 +53,7 @@ IK_CONFIG_DICT = {
         "tienkung": IK_CONFIG_ROOT / "smplx_to_tienkung.json",
         "fourier_gr3": IK_CONFIG_ROOT / "smplx_to_gr3.json",
     },
-    "bvh_lafan1":{
+    "bvh_lafan1": {
         "unitree_g1": IK_CONFIG_ROOT / "bvh_lafan1_to_g1.json",
         "unitree_g1_with_hands": IK_CONFIG_ROOT / "bvh_lafan1_to_g1.json",
         "booster_t1_29dof": IK_CONFIG_ROOT / "bvh_lafan1_to_t1_29dof.json",
@@ -54,23 +61,23 @@ IK_CONFIG_DICT = {
         "stanford_toddy": IK_CONFIG_ROOT / "bvh_lafan1_to_toddy.json",
         "engineai_pm01": IK_CONFIG_ROOT / "bvh_lafan1_to_pm01.json",
         "pal_talos": IK_CONFIG_ROOT / "bvh_to_talos.json",
+        "apptronik_apollo": IK_CONFIG_ROOT / "bvh_lafan1_to_apollo.json",
     },
-    "bvh_nokov":{
+    "bvh_nokov": {
         "unitree_g1": IK_CONFIG_ROOT / "bvh_nokov_to_g1.json",
     },
-    "bvh_xsens":{
+    "bvh_xsens": {
         "unitree_g1": IK_CONFIG_ROOT / "bvh_xsens_to_g1.json",
         "unitree_h1_2": IK_CONFIG_ROOT / "bvh_xsens_to_h1_2.json",
     },
-    "fbx":{
+    "fbx": {
         "unitree_g1": IK_CONFIG_ROOT / "fbx_to_g1.json",
         "unitree_g1_with_hands": IK_CONFIG_ROOT / "fbx_to_g1.json",
     },
-    "fbx_offline":{
+    "fbx_offline": {
         "unitree_g1": IK_CONFIG_ROOT / "fbx_offline_to_g1.json",
     },
-    
-    "xrobot":{
+    "xrobot": {
         "unitree_g1": IK_CONFIG_ROOT / "xrobot_to_g1.json",
     },
     "xsens_mvn": {
@@ -98,6 +105,7 @@ ROBOT_BASE_DICT = {
     "tienkung": "Base_link",
     "pal_talos": "base_link",
     "fourier_gr3": "base_link",
+    "apptronik_apollo": "base_link",
 }
 
 VIEWER_CAM_DISTANCE_DICT = {
@@ -119,4 +127,5 @@ VIEWER_CAM_DISTANCE_DICT = {
     "tienkung": 3.0,
     "pal_talos": 3.0,
     "fourier_gr3": 2.0,
+    "apptronik_apollo": 3.0,
 }
